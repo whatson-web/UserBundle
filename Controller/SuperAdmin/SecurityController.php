449dc81fcaefd471d9ac565f727f37675ce83094
JF <jf@whatson-web.com>
2017-05-04 16:17:18 +0200

    CS

--- Controller/SuperAdmin/SecurityController.php
+++ Controller/SuperAdmin/SecurityController.php
@@ -19,79 +19,79 @@ use Symfony\Component\Security\Core\Security;
 class SecurityController extends BaseController
 {
 
-	/**
-	 * @Route("/login", name="sudo_wh_user_security_login")
-	 *
-	 * @param Request $request
-	 *
-	 * @return Response
-	 */
-	public function loginAction(Request $request)
-	{
-		/** @var $session \Symfony\Component\HttpFoundation\Session\Session */
-		$session = $request->getSession();
+    /**
+     * @Route("/login", name="sudo_wh_user_security_login")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function loginAction(Request $request)
+    {
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
 
-		$authErrorKey = Security::AUTHENTICATION_ERROR;
-		$lastUsernameKey = Security::LAST_USERNAME;
+        $authErrorKey = Security::AUTHENTICATION_ERROR;
+        $lastUsernameKey = Security::LAST_USERNAME;
 
-		// get the error if any (works with forward and redirect -- see below)
-		if ($request->attributes->has($authErrorKey)) {
-			$error = $request->attributes->get($authErrorKey);
-		} elseif (null !== $session && $session->has($authErrorKey)) {
-			$error = $session->get($authErrorKey);
-			$session->remove($authErrorKey);
-		} else {
-			$error = null;
-		}
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has($authErrorKey)) {
+            $error = $request->attributes->get($authErrorKey);
+        } elseif (null !== $session && $session->has($authErrorKey)) {
+            $error = $session->get($authErrorKey);
+            $session->remove($authErrorKey);
+        } else {
+            $error = null;
+        }
 
-		if (!$error instanceof AuthenticationException) {
-			$error = null; // The value does not come from the security component.
-		}
+        if (!$error instanceof AuthenticationException) {
+            $error = null; // The value does not come from the security component.
+        }
 
-		// last username entered by the user
-		$lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
 
-		$csrfToken = $this->has('security.csrf.token_manager')
-			? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
-			: null;
+        $csrfToken = $this->has('security.csrf.token_manager')
+            ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
+            : null;
 
-		return $this->renderLogin(
-			array(
-				'last_username' => $lastUsername,
-				'error'         => $error,
-				'csrf_token'    => $csrfToken,
-			)
-		);
-	}
+        return $this->renderLogin(
+            [
+                'last_username' => $lastUsername,
+                'error'         => $error,
+                'csrf_token'    => $csrfToken,
+            ]
+        );
+    }
 
-	/**
-	 * Renders the login template with the given parameters. Overwrite this function in
-	 * an extended controller to provide additional data for the login template.
-	 *
-	 * @param array $data
-	 *
-	 * @return Response
-	 */
-	protected function renderLogin(array $data)
-	{
-		return $this->render('WHUserBundle:SuperAdmin/Security:login.html.twig', $data);
-	}
+    /**
+     * Renders the login template with the given parameters. Overwrite this function in
+     * an extended controller to provide additional data for the login template.
+     *
+     * @param array $data
+     *
+     * @return Response
+     */
+    protected function renderLogin(array $data)
+    {
+        return $this->render('WHUserBundle:SuperAdmin/Security:login.html.twig', $data);
+    }
 
-	/**
-	 * @Route("/login_check", name="sudo_wh_user_security_logincheck")
-	 */
-	public function checkAction()
-	{
-		throw new \RuntimeException(
-			'You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.'
-		);
-	}
+    /**
+     * @Route("/login_check", name="sudo_wh_user_security_logincheck")
+     */
+    public function checkAction()
+    {
+        throw new \RuntimeException(
+            'You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.'
+        );
+    }
 
-	/**
-	 * @Route("/logout", name="sudo_wh_user_security_logout")
-	 */
-	public function logoutAction()
-	{
-		throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
-	}
+    /**
+     * @Route("/logout", name="sudo_wh_user_security_logout")
+     */
+    public function logoutAction()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
 }
--- Mailer/BackendMailer.php
+++ Mailer/BackendMailer.php
@@ -15,57 +15,57 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 class BackendMailer extends TwigSwiftMailer implements MailerInterface
 {
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function sendConfirmationEmailMessage(UserInterface $user)
-	{
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function sendConfirmationEmailMessage(UserInterface $user)
+    {
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function sendResettingPasswordMessage(UserInterface $user, $newPassword)
-	{
-		$context = array(
-			'user'        => $user,
-			'loginUrl'    => $this->router->generate(
-				'bk_wh_user_security_login',
-				array(),
-				UrlGeneratorInterface::ABSOLUTE_URL
-			),
-			'newPassword' => $newPassword,
-		);
+    /**
+     * {@inheritdoc}
+     */
+    public function sendResettingPasswordMessage(UserInterface $user, $newPassword)
+    {
+        $context = [
+            'user'        => $user,
+            'loginUrl'    => $this->router->generate(
+                'bk_wh_user_security_login',
+                [],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            ),
+            'newPassword' => $newPassword,
+        ];
 
-		$this->sendMessage(
-			'@WHUser/Backend/Email/resetting-new-password.html.twig',
-			$context,
-			$this->parameters['from_email']['resetting'],
-			(string)$user->getEmail()
-		);
-	}
+        $this->sendMessage(
+            '@WHUser/Backend/Email/resetting-new-password.html.twig',
+            $context,
+            $this->parameters['from_email']['resetting'],
+            (string)$user->getEmail()
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function sendResettingEmailMessage(UserInterface $user)
-	{
-		$url = $this->router->generate(
-			'bk_wh_user_resetting_reset',
-			array('token' => $user->getConfirmationToken()),
-			UrlGeneratorInterface::ABSOLUTE_URL
-		);
+    /**
+     * {@inheritdoc}
+     */
+    public function sendResettingEmailMessage(UserInterface $user)
+    {
+        $url = $this->router->generate(
+            'bk_wh_user_resetting_reset',
+            ['token' => $user->getConfirmationToken()],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
 
-		$context = array(
-			'user'            => $user,
-			'confirmationUrl' => $url,
-		);
+        $context = [
+            'user'            => $user,
+            'confirmationUrl' => $url,
+        ];
 
-		$this->sendMessage(
-			'@WHUser/Backend/Email/resetting-link-email.html.twig',
-			$context,
-			$this->parameters['from_email']['resetting'],
-			(string)$user->getEmail()
-		);
-	}
+        $this->sendMessage(
+            '@WHUser/Backend/Email/resetting-link-email.html.twig',
+            $context,
+            $this->parameters['from_email']['resetting'],
+            (string)$user->getEmail()
+        );
+    }
 }
